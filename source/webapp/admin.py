663930d188d7073204fd0308289217be7b8eb236
asankul <asankul1993@gmail.com>
2019-09-24 16:47:56 +0600

    Created model of Products

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from webapp.models import Products
 
-# Register your models here.
+
+class ProductsAdmin(admin.ModelAdmin):
+
+    list_display = ['name', 'description', 'category', 'count', 'price']
+    list_filter = ['name', 'category', 'price']
+    search_fields = ['name', 'category']
+    fields = ['name', 'description', 'category', 'count', 'price']
+
+
+admin.site.register(Products, ProductsAdmin)
+
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+CATEGORY_CHOICES = [('other', 'Разное'), ('cat_1', 'Категория 1'), ('cat_2', 'Категория 2'), ('cat_3', 'Категория 3')]
+
+
+class Products(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False, verbose_name='Название')
+    description = models.TextField(max_length=3000, null=True, blank=True, verbose_name='Описание')
+    category = models.CharField(choices=CATEGORY_CHOICES, max_length=200, default='other')
+    count = models.PositiveIntegerField(verbose_name='Остаток')
+    price = models.DecimalField(max_digits=7, decimal_places=2, verbose_name='Цена')
+
+    def __str__(self):
+        return "{}. {}".format(self.pk, self.name)
