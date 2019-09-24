3df5d42da6b1851bb5ba43fc76b95d7e614c3d50
asankul <asankul1993@gmail.com>
2019-09-24 17:41:11 +0600

    Created Main Page and Product view page

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from webapp.models import Products
 
-# Create your views here.
+
+def products_view(request, *args, **kwargs):
+    products = Products.objects.filter(count__gt=0).order_by('category', 'name')
+    context = {
+        'products': products,
+    }
+    return render(request, 'index.html', context)
+
+
+def product_detail_view(request, pk):
+    product = get_object_or_404(Products, pk=pk)
+
+    return render(request, 'product.html', context={
+        'product': product
+    })
